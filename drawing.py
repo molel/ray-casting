d12431e024438deec7c8b2cb77a0c62280e8f2c4
molel <kapitov.04@gmail.com>
2022-03-15 20:37:24 +0300

    Added textures

--- drawing.py
+++ drawing.py
@@ -9,6 +9,8 @@ class Drawing:
     def __init__(self, screen, walls):
         self.screen = screen
         self.walls = walls
+        self.img = pg.image.load("img/wall.png").convert()
+        self.textures = {0: pg.image.load("img/wall.png").convert()}
 
     @staticmethod
     def mapping(x, y):
@@ -29,28 +31,31 @@ class Drawing:
             sin_p = sin_p if sin_p else 0.00001
             cos_p = cos_p if cos_p else 0.00001
             # Vertical intersections
-            x, dx = (mx + TILE_SIZE, 1) if cos_p >= 0 else (mx, -1)
+            xv, dx = (mx + TILE_SIZE, 1) if cos_p >= 0 else (mx, -1)
             for i in range(0, WIDTH, TILE_SIZE):
-                dv = (x - ox) / cos_p
+                dv = (xv - ox) / cos_p
                 y = oy + dv * sin_p
-                if self.mapping(x + dx, y) in self.walls:
+                if self.mapping(xv + dx, y) in self.walls:
                     break
-                x += dx * TILE_SIZE
+                xv += dx * TILE_SIZE
 
             # Horizontal intersections
-            y, dy = (my + TILE_SIZE, 1) if sin_p >= 0 else (my, -1)
+            yh, dy = (my + TILE_SIZE, 1) if sin_p >= 0 else (my, -1)
             for i in range(0, WIDTH, TILE_SIZE):
-                dh = (y - oy) / sin_p
+                dh = (yh - oy) / sin_p
                 x = ox + dh * cos_p
-                if self.mapping(x, y + dy) in self.walls:
+                if self.mapping(x, yh + dy) in self.walls:
                     break
-                y += dy * TILE_SIZE
+                yh += dy * TILE_SIZE
 
-            depth = min(dv, dh)
+            depth, index = (dv, y % TILE_SIZE) if dv <= dh else (dh, x % TILE_SIZE)
             depth *= cos(opov - pov)
             depth = max(depth, 0.00001)
             proj_height = min(PROJ_COEFF / depth, 2 * HEIGHT)
             c = 255 / (1 + depth * depth * 0.00002)
             color = (c, c, c)
-            pg.draw.rect(self.screen, color, (ray * SCALE_X, CENTER_HEIGHT - proj_height // 2, SCALE_X, proj_height))
+            tempSurf = pg.Surface((SCALE_X, HEIGHT))
+            tempSurf.blit(self.img, (0, 0), (index * SCALE_TEXTURE, 0, SCALE_TEXTURE, 800))
+            tempSurf = pg.transform.scale(tempSurf, (SCALE_X, proj_height))
+            self.screen.blit(tempSurf, (ray * SCALE_X, CENTER_HEIGHT - proj_height // 2))
             pov += RAY_STEP
